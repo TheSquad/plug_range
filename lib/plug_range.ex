4e03405f536aa539c155967cc7fbd260f9f1b72f
TheSquad <msegalis@gmail.com>
2016-04-14 23:52:22 +0200

    PlugRange Added some unhandled case
    Test
    Added some Test

--- lib/plug_range.ex
+++ lib/plug_range.ex
@@ -17,32 +17,59 @@ defmodule PlugRange do
         stats = File.stat! file_path
         filesize = stats.size
 
-        req = Regex.run(~r/bytes=([0-9]+)-([0-9]+)?/, conn |> Plug.Conn.get_req_header("range") |> List.first)
+        req = Regex.run(~r/bytes=([0-9]+)?-([0-9]+)?/, conn |> Plug.Conn.get_req_header("range") |> List.first)
 
-        {req_start, _} = req |> Enum.at(1) |> Integer.parse
-        {req_end, _} = req |> Enum.at(2, filesize |> to_string) |> Integer.parse
+        case req do
+          p when p == nil or ["bytes=", "", "-"] == p or length(p) < 2 ->
+            send_bad_range conn
+          _ ->
+            {req_start, _} =
+            if (req |> Enum.at(1) == "") do
+              {-1, :end_file}
+            else
+              req |> Enum.at(1)  |> Integer.parse
+            end
+            {req_end, _} = req |> Enum.at(2, filesize |> to_string) |> Integer.parse
 
+            if (req_start == 0 && req_end == filesize) do
+              conn
+            else
 
-        IO.puts "req_end : #{req_end} // filesize = #{filesize}"
+              {req_start, length} = case req_start do
+                                      -1 -> {filesize - req_end, req_end}
+                                      p -> {req_start, req_end - req_start + 1}
+                                    end
 
-        if (req_start == 0 && req_end == filesize) do
+              file_end = ( filesize - 2) |> to_string
 
-          conn
-        else
-
-          file_end = ( filesize - 2) |> to_string
-
-          length = req_end - req_start + 1
-
-          conn
-          |> Plug.Conn.put_resp_header("Accept-Ranges", "bytes")
-          |> Plug.Conn.put_resp_header("Content-Range", "bytes #{req_start}-#{req_end}/#{filesize}")
-          |> Plug.Conn.send_file(206, file_path, req_start, length)
-          |> Plug.Conn.halt
+              conn
+              |> Plug.Conn.put_resp_header("accept-ranges", "bytes")
+              |> Plug.Conn.put_resp_header("content-range", "bytes #{req_start}-#{req_end}/#{filesize}")
+              |> Plug.Conn.send_file(206, file_path, req_start, length)
+              |> Plug.Conn.halt
+            end
         end
-      else
-        conn
       end
     end
+  end
+
+  def send_bad_range(conn) do
+    conn
+    |> Plug.Conn.put_resp_content_type("text/html")
+    |> Plug.Conn.put_resp_header("content-length", bad_range |> String.length |> to_string)
+    |> Plug.Conn.send_resp(416, bad_range)
+    |> Plug.Conn.halt
+  end
+
+  def bad_range do
+    """
+    <html>
+    <head><title>416 Requested Range Not Satisfiable</title></head>
+    <body bgcolor="white">
+    <center><h1>416 Requested Range Not Satisfiable</h1></center>
+    <hr><center>cowboy</center>
+    </body>
+    </html>
+    """
   end
 end
--- test/plug_range_test.exs
+++ test/plug_range_test.exs
@@ -1,8 +1,79 @@
 defmodule PlugRangeTest do
   use ExUnit.Case
+  use Plug.Test
   doctest PlugRange
 
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  @opts PlugRange.init([])
+
+  test "no range request" do
+    conn = conn(:get, "/lorem.txt")
+    conn_changed? = PlugRange.call(conn, @opts)
+    assert conn == conn_changed?
+  end
+
+  test "range request 0-" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "bytes=0-")
+
+    conn_changed? = PlugRange.call(conn, @opts)
+    assert conn == conn_changed?
+  end
+
+  test "range request 0-32" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "bytes=0-32")
+
+    conn = PlugRange.call conn, @opts
+
+    assert conn.state == :sent
+    assert conn.status == 206
+    res_string = File.stream!("priv/static/lorem.txt", [], 33) |> Stream.take(1) |> Enum.to_list |> to_string
+    assert conn.resp_body == res_string
+  end
+
+  test "range request -32" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "bytes=-32")
+
+    conn = PlugRange.call conn, @opts
+
+    assert conn.state == :sent
+    assert conn.status == 206
+    fs = File.stat! "priv/static/lorem.txt"
+    res_string = File.read!("priv/static/lorem.txt") |> String.slice(fs.size - 32, 32)
+
+    assert conn.resp_body == res_string
+  end
+
+  test "bad range request" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "bytes=-")
+
+    conn = PlugRange.call conn, @opts
+    assert conn.state == :sent
+    assert conn.status == 416
+  end
+
+  test "bad range request 2" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "bytes=toto")
+
+    conn = PlugRange.call conn, @opts
+    assert conn.state == :sent
+    assert conn.status == 416
+  end
+
+  test "bad range request 3" do
+    conn = conn(:get, "/lorem.txt")
+    |> Plug.Conn.put_req_header("range", "by")
+
+    conn = PlugRange.call conn, @opts
+    assert conn.state == :sent
+    assert conn.status == 416
+  end
+
 end
